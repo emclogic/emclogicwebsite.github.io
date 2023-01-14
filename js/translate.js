52a1a2ab804339ad3dd1710922456cdb4b624375
ferlzc <ferlzc@gmail.com>
2023-01-13 23:18:16 -0300

    adding translations

--- js/translate.js
+++ js/translate.js
@@ -1,24 +1,53 @@
-$.getJSON("./pt_br", function (data) {
-    const pt_br = data
-});
+const pt_br = {
+    "content": {
+        "init_txt": "Início",
+        "about_txt": "Sobre",
+        "services_txt": "Serviços",
+        "area_txt": "Áreas",
+        "tec_txt": "Tecnologias",
+        "contact_txt": "Contato"
+    }
+}
 
-$.getJSON("./en_us", function (data) {
-    const en_us = data
-});
-
+const en_us = {
+    "content": {
+        "init_txt": "Begin",
+        "about_txt": "About",
+        "services_txt": "Services",
+        "area_txt": "Areas",
+        "tec_txt": "Tecnologies",
+        "contact_txt": "Contact"
+    }
+}
 
 $("#document").ready(function () {
 
-    if ( $( "#button_br" ).click(function () {
 
-        $("#init_txt").val(pt_br[content][init_txt]);
 
-    }) );
+    $("#button_br").click(function () {
 
-    else ( $("#english_br").click(function () {
+        // PT_BR
 
-        $("#init_txt").val(en_us[content][init_txt]);
+        $("#init_txt").html(pt_br.content.init_txt);
+        $("#about_txt").html(pt_br.content.about_txt);
+        $("#services_txt").html(pt_br.content.services_txt);
+        $("#area_txt").html(pt_br.content.area_txt);
+        $("#tec_txt").html(pt_br.content.tec_txt);
+        $("#contact_txt").html(pt_br.content.contact_txt);
 
-    }) ); 
+    })
+
+     $("#button_us").click(function () {
+
+        // EN_US 
+
+        $("#init_txt").html(en_us.content.init_txt);
+        $("#about_txt").html(en_us.content.about_txt);
+        $("#services_txt").html(en_us.content.services_txt);
+        $("#area_txt").html(en_us.content.area_txt);
+        $("#tec_txt").html(en_us.content.tec_txt);
+        $("#contact_txt").html(en_us.content.contact_txt);
+        
+    }) 
 
 });
