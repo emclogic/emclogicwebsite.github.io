b189cc083da33029f867df6f1d344aac22e866f8
bjlli <brendajacomelli@gmail.com>
2023-01-16 16:55:27 -0300

    tradução para inglês

--- js/translate.js
+++ js/translate.js
@@ -1,22 +1,134 @@
 const pt_br = {
     "content": {
+        "main_title": "Emc Logic - Viabilizando Tecnologias Embarcadas",
+        /*Barra de Navegação*/
+        "logo_navbar_text": "Viabilizando Tecnologias Embarcadas",
         "init_txt": "Início",
         "about_txt": "Sobre",
         "services_txt": "Serviços",
         "area_txt": "Áreas",
         "tec_txt": "Tecnologias",
-        "contact_txt": "Contato"
+        "contact_txt": "Contato",
+        /* Início */
+        "image_home_text": "Consultoria, Treinamento e Desenvolvimento de Software Embarcado para produtos inovadores.",
+        /* Sobre */
+        "about_title": "Sobre a Emc Logic",
+        "about_text": "Fundada em 2017, a Emc Logic é uma empresa que presta serviços de consultoria, treinamento e desenvolvimento de software para Sistemas Embarcados microcontrolados e microprocessados. Viabilizamos um rápido time to market para dispositivos embarcados incorporando as melhores práticas de desenvolvimento e migrando projetos para os mais atuais softwares open source. Atuamos com projetos de diversas áreas, de equipamentos médicos até tecnologias agrícolas, e estamos sempre dispostos a assessorar nossos clientes para que encontrem soluções rápidas, otimizadas, seguras e com um bom custo benefício.",
+        /* Serviços */
+        "services_title": "Serviços",
+            /* Linux */
+        "linux_title": "Desenvolvimento de Software para Linux Embarcado",
+        "linux_descrip": "Desenvolvimento de Software em Linux Embarcado para dipositivos que demandam interfaces ricas, assim como alta conectividade e necessidade de integração com diversos periféricos.", 
+        "linux_l1": "<small>Programação em <strong>Assembly, C/C++, Python e Shell script.</strong></small>",
+        "linux_l2": "<small>Personalização e otimização de imagens com <strong>Yocto Project e Buildroot.</strong></small>",
+        "linux_l3": "<small>Desenvolvimento de interfaces gráficas para <strong>displays TFT ou LVDS.</strong></small>",
+        "linux_l4": "<small>Otimização do <strong>tempo de Boot.</strong></small>",
+        "linux_l5": "<small>Desenvolvimento de <strong>BSPs.</strong></small>",
+        "linux_l6": "<small>Desenvolvimento de <strong>drivers de dispositivos</strong> Linux.</small>",
+        "linux_l7": "<small>Modificação e configuração de <strong>Device-Tree</strong> para processadores ARM e RISC-V.</small>",
+        "linux_l8": "<small>Preparação do <strong>ambiente de programação e compilação cruzada.</strong></small>",
+            /* Microcontroladores */
+        "firmw_title": "Desenvolvimento de Firmware para Microcontroladores",
+        "firmw_descrip": "Desenvolvimento de firmware para microcontroladores de 8 a 64 bits (STM32, Microchip, Texas, NXP, Renesas e etc) garantindo o melhor desempenho para o seu produto.",
+        "firmw_l1":"<small>Firmware para sistemas em <strong>Baremetal ou RTOS.</strong></small>",
+        "firmw_l2":"<small>Redução de <strong>footprint</strong> em memória e <strong>otimização</strong> do consumo de energia.</small>",
+        "firmw_l3":"<small>Desenvolvimento de <strong>drivers e bibliotecas de dispositivos.</strong></small>",
+        "firmw_l4":"<small>Configuração de periféricos como <strong>UART, I2C, PWM, GPIO, DMA e CAN.</strong></small>",
+        "firmw_l5":"<small>Arquitetura modular e desenvolvimento com camadas de abstração - <strong>HAL</strong></small>",
+        "firmw_l6":"<small>Analisadores estáticos de código e testes unitários.</small>",
+        "firmw_l7":"<small><strong>Versionamento</strong> de código e <strong>deploy seguro.</strong></small>",
+            /* Consultoria */
+        "consul_title": "Consultoria e Treinamento sob Demanda",
+        "consul_descrip": "Assessoramento para o seu projeto a fim de encontrar estratégias de desenvolvolvimento que estejam alinhandas com as necessidades do mercado. Treinamentos sob demanda da equipe e gestão de conhecimento.",
+        "consul_l1":"<small>Análise da melhor <strong>arquitetura de hardware </strong> para seu projeto.</small>",
+        "consul_l2":"<small>Escolha das melhores <strong>ferramentas e tecnologias </strong> para seu projeto.</small>",
+        "consul_l3":"<small>Adequação da melhor estratégia para desenvolvimento de software embarcado <strong>rápido e seguro.</strong></small>",
+        "consul_l4":"<small>Documentação e Gestão de conhecimento.</small>",
+        "consul_l5":"<small>Assessoria referente à <strong>licenças de software livre.</strong></small>",
+        "consul_l6":"<small>Treinamentos <strong>online e presenciais</strong> sob demanda da equipe do cliente.</small>",
+        "consul_l7":"<small>Internalização gradual do desenvolvimento pela equipe do cliente.</small>",
+        /* Áreas */
+        "areas_title": "<strong>Áreas de atuação</strong>",
+        "area_indust":"<strong>Máquinas Industriais</strong>",
+        "area_agric":"<strong>Tecnologias Agrícolas</strong>",
+        "area_acces":"<strong>Controle de Acesso</strong>",
+        "area_med":"<strong>Equipamentos Médicos</strong>",
+        "area_lab":"<strong>Equipamentos de Laboratórios</strong>",
+        "area_iot":"<strong>Internet das Coisas (IoT)</strong>",
+        /* Tecnologias */
+        "tec_title" : "Principais Tecnologias",
+        /* Rodapé */
+        "logo_fnote_text": "Viabilizando Tecnologias Embarcadas",
+        "local_title": "<i class=\"fa fa-location-arrow\"></i>&emsp;Localização :",
+        "phone_title": "<i class=\"fa fa-location-arrow\"></i>&emsp;Telefone :",
+        "itapolis_text": "Itápolis - SP - Brasil", 
     }
 }
 
 const en_us = {
     "content": {
+        "main_title": "Emc Logic - Making Embedded Technologies Easier",
+        /*Navbar*/
+        "logo_navbar_text": "Making Embedded Technologies Easier",
         "init_txt": "Begin",
         "about_txt": "About",
         "services_txt": "Services",
         "area_txt": "Areas",
-        "tec_txt": "Tecnologies",
-        "contact_txt": "Contact"
+        "tec_txt": "Technologies",
+        "contact_txt": "Contact",
+        /* Home */
+        "image_home_text": "Consulting, Training and Software Development for inovative Embedded products.",
+        /* About */
+        "about_title": "About us",
+        "about_text": "Emc Logic was founded in  2017 and It’s a company that offers services of consulting, training and software development for microcontrolled and microprocessed Embedded Systems. We make a fast time to market for this devices applying the best development practices and migrating projects to latest open source softwares. We work with projects of different areas , going throught medical equipments to agricultural technologies. We are willing to help our clients to find fast, optimized and safe solutions.",
+        /* Services */
+        "services_title": "Services",
+            /* Linux */
+        "linux_title": "Software Development for Embedded Linux",
+        "linux_descrip": "Embedded Linux Software development for devices that require complex interfaces, high connectivity and necessity of integration with tons of peripherals.",
+        "linux_l1": "<small><strong>Assembly, C/C++, Python e Shell script</strong> programming.</small>",
+        "linux_l2": "<small>Customization and optimization of os images using<strong>Yocto Project e Buildroot.</strong></small>", 
+        "linux_l3": "<small>Graphic interfaces development for <strong>TFT displays or LVDS.</strong></small>",
+        "linux_l4": "<small><strong>Boot time </strong> optimization.</small>",
+        "linux_l5": "<small><strong>BSPs</strong> development</small>",
+        "linux_l6": "<small>Linux <strong>Device Drivers</strong> development</small>",
+        "linux_l7": "<small><strong>Device-Tree</strong> reconfiguration for ARM e RISC-V processors.</small>",
+        "linux_l8": "<small>Preparation of <strong>Development Environments and cross-compilation.</strong></small>",
+            /* Microcontrollers */
+        "firmw_title": "Firmware Development for Microcontrollers",
+        "firmw_descrip": "Firmware Development for 8 to 64 bits microcontrollers (STM32,Microchip, Texas, NXP, Renesas, etc) making sure that your product achieves the best performance.",
+        "firmw_l1":"<small> <strong>Baremetal or RTOS</strong> firmware development</small>",
+        "firmw_l2":"<small>Memory<strong>footprint</strong> reduction and <strong>energy consumption</strong>optimization.</small>",
+        "firmw_l3":"<small> Device <strong>drivers and libraries</strong> development.</small>",
+        "firmw_l4":"<small><strong>UART, I2C, PWM, GPIO, DMA e CAN</strong> configuration.</small>",
+        "firmw_l5":"<small>Modular architecture and software development with abstraction <strong>HAL.</strong></small>",
+        "firmw_l6":"<small>Code static analysis and unit tests.</small>",
+        "firmw_l7":"<small>Code <strong>versioning</strong>and <strong> safe deploy.</strong></small>",
+            /* Consulting */
+        "consul_title": "Consulting and Training on demand",
+        "consul_descrip": "Consulting for your project in order to find development strategies that are aligned with the market requirements. Training on your team demand and knowledge management.",
+        "consul_l1":"<small>Analysis of the best <strong>hardware architecture</strong> for your project.</small>",
+        "consul_l2":"<small>Selection of the best<strong>technologies </strong> for your project.</small>",
+        "consul_l3":"<small>Strategies for a <strong>fast and safe</strong> embedded software development.</small>",
+        "consul_l4":"<small>Knowledge documentantion and managemant.</small>",
+        "consul_l5":"<small>Consulting regarding <strong>open source licenses.</strong></small>",
+        "consul_l6":"<small><strong>Online and in person</strong> training on team demand.</small>",
+        "consul_l7":"<small>Gradual internalization of development.</small>",
+        /* Areas */
+        "areas_title": "Areas",
+        "area_indust":"<strong>Industrial Machinery</strong>",
+        "area_agric":"<strong>Agriculture Technology</strong>",
+        "area_acces":"<strong>Access Control</strong>",
+        "area_med":"<strong>Medical Equipment</strong>",
+        "area_lab":"<strong>Laboratory Equipment</strong>",
+        "area_iot":"<strong>Internet of Things (IoT)</strong>",
+        /* Technologies */
+        "tec_title" : "Technologies",
+        /* Footnote */
+        "logo_fnote_text": "Making Embedded Technologies Easier",
+        "local_title": "<i class=\"fa fa-location-arrow\"></i>&emsp;Localization :",
+        "phone_title": "<i class=\"fa fa-location-arrow\"></i>&emsp;Phone :",
+        "itapolis_text": "Itápolis - SP - Brazil", 
     }
 }
 
@@ -27,26 +139,137 @@ $("#document").ready(function () {
     $("#button_br").click(function () {
 
         // PT_BR
-
+        $("#main_title").html(pt_br.content.main_title);
+        /* Barra de Navegação */
+        $("#logo_navbar_text").html(pt_br.content.logo_navbar_text);
         $("#init_txt").html(pt_br.content.init_txt);
         $("#about_txt").html(pt_br.content.about_txt);
         $("#services_txt").html(pt_br.content.services_txt);
         $("#area_txt").html(pt_br.content.area_txt);
         $("#tec_txt").html(pt_br.content.tec_txt);
         $("#contact_txt").html(pt_br.content.contact_txt);
-
+        /* Início */
+        $("#image_home_text").html(pt_br.content.image_home_text);
+        /* Sobre */
+        $("#about_title").html(pt_br.content.about_title);
+        $("#about_text").html(pt_br.content.about_text);
+        /* Serviços */
+        $("#services_title").html(pt_br.content.services_title);
+            /* Linux */
+        $("#linux_title").html(pt_br.content.linux_title);
+        $("#linux_descrip").html(pt_br.content.linux_descrip);
+        $("#linux_l1").html(pt_br.content.linux_l1);
+        $("#linux_l2").html(pt_br.content.linux_l2);
+        $("#linux_l3").html(pt_br.content.linux_l3);
+        $("#linux_l4").html(pt_br.content.linux_l4);
+        $("#linux_l5").html(pt_br.content.linux_l5);
+        $("#linux_l6").html(pt_br.content.linux_l6);
+        $("#linux_l7").html(pt_br.content.linux_l7);
+        $("#linux_l8").html(pt_br.content.linux_l8);
+            /* Microcontroladores */
+        $("#firmw_title").html(pt_br.content.firmw_title);
+        $("#firmw_descrip").html(pt_br.content.firmw_descrip);
+        $("#firmw_l1").html(pt_br.content.firmw_l1);
+        $("#firmw_l2").html(pt_br.content.firmw_l2);
+        $("#firmw_l3").html(pt_br.content.firmw_l3);
+        $("#firmw_l4").html(pt_br.content.firmw_l4);
+        $("#firmw_l5").html(pt_br.content.firmw_l5);
+        $("#firmw_l6").html(pt_br.content.firmw_l6);
+        $("#firmw_l7").html(pt_br.content.firmw_l7);
+            /* Consultoria */
+        $("#consul_title").html(pt_br.content.consul_title);
+        $("#consul_descrip").html(pt_br.content.consul_descrip);
+        $("#consul_l1").html(pt_br.content.consul_l1);
+        $("#consul_l2").html(pt_br.content.consul_l2);
+        $("#consul_l3").html(pt_br.content.consul_l3);
+        $("#consul_l4").html(pt_br.content.consul_l4);
+        $("#consul_l5").html(pt_br.content.consul_l5);
+        $("#consul_l6").html(pt_br.content.consul_l6);
+        $("#consul_l7").html(pt_br.content.consul_l7);
+        /* Áreas */
+        $("#areas_title").html(pt_br.content.areas_title);
+        $("#area_indust").html(pt_br.content.area_indust);
+        $("#area_agric").html(pt_br.content.area_agric);
+        $("#area_acces").html(pt_br.content.area_acces);
+        $("#area_med").html(pt_br.content.area_med);
+        $("#area_lab").html(pt_br.content.area_lab);
+        $("#area_iot").html(pt_br.content.area_iot);
+        /* Tecnologias */
+        $("#tec_title").html(pt_br.content.tec_title);
+        /* Rodapé */
+        $("#logo_fnote_text").html(pt_br.content.logo_fnote_text);
+        $("#local_title").html(pt_br.content.local_title);
+        $("#phone_title").html(pt_br.content.phone_title);
+        $("#itapolis_text").html(pt_br.content.itapolis_text);
+        
     })
 
      $("#button_us").click(function () {
 
         // EN_US 
-
+        $("#main_title").html(en_us.content.main_title);
+        /* Navbar */
+        $("#logo_navbar_text").html(en_us.content.logo_navbar_text);
         $("#init_txt").html(en_us.content.init_txt);
         $("#about_txt").html(en_us.content.about_txt);
         $("#services_txt").html(en_us.content.services_txt);
         $("#area_txt").html(en_us.content.area_txt);
         $("#tec_txt").html(en_us.content.tec_txt);
         $("#contact_txt").html(en_us.content.contact_txt);
+        /* Home */
+        $("#image_home_text").html(en_us.content.image_home_text);
+        /* About */
+        $("#about_title").html(en_us.content.about_title);
+        $("#about_text").html(en_us.content.about_text);
+        /* Services */
+        $("#services_title").html(en_us.content.services_title);
+            /* Linux */
+        $("#linux_title").html(en_us.content.linux_title);
+        $("#linux_descrip").html(en_us.content.linux_descrip);
+        $("#linux_l1").html(en_us.content.linux_l1);
+        $("#linux_l2").html(en_us.content.linux_l2);
+        $("#linux_l3").html(en_us.content.linux_l3);
+        $("#linux_l4").html(en_us.content.linux_l4);
+        $("#linux_l5").html(en_us.content.linux_l5);
+        $("#linux_l6").html(en_us.content.linux_l6);
+        $("#linux_l7").html(en_us.content.linux_l7);
+        $("#linux_l8").html(en_us.content.linux_l8);
+            /* Microcontrollers */
+        $("#firmw_title").html(en_us.content.firmw_title);
+        $("#firmw_descrip").html(en_us.content.firmw_descrip);
+        $("#firmw_l1").html(en_us.content.firmw_l1);
+        $("#firmw_l2").html(en_us.content.firmw_l2);
+        $("#firmw_l3").html(en_us.content.firmw_l3);
+        $("#firmw_l4").html(en_us.content.firmw_l4);
+        $("#firmw_l5").html(en_us.content.firmw_l5);
+        $("#firmw_l6").html(en_us.content.firmw_l6);
+        $("#firmw_l7").html(en_us.content.firmw_l7);
+            /* Consulting */
+        $("#consul_title").html(en_us.content.consul_title);
+        $("#consul_descrip").html(en_us.content.consul_descrip);
+        $("#consul_l1").html(en_us.content.consul_l1);
+        $("#consul_l2").html(en_us.content.consul_l2);
+        $("#consul_l3").html(en_us.content.consul_l3);
+        $("#consul_l4").html(en_us.content.consul_l4);
+        $("#consul_l5").html(en_us.content.consul_l5);
+        $("#consul_l6").html(en_us.content.consul_l6);
+        $("#consul_l7").html(en_us.content.consul_l7);
+        /* Areas */
+        $("#areas_title").html(en_us.content.areas_title);
+        $("#areas_title").html(en_us.content.areas_title);
+        $("#area_indust").html(en_us.content.area_indust);
+        $("#area_agric").html(en_us.content.area_agric);
+        $("#area_acces").html(en_us.content.area_acces);
+        $("#area_med").html(en_us.content.area_med);
+        $("#area_lab").html(en_us.content.area_lab);
+        $("#area_iot").html(en_us.content.area_iot);
+        /* Technologies */
+        $("#tec_title").html(en_us.content.tec_title);
+        /* Footnote */
+        $("#logo_fnote_text").html(en_us.content.logo_fnote_text);
+        $("#local_title").html(en_us.content.local_title);
+        $("#phone_title").html(en_us.content.phone_title);
+        $("#itapolis_text").html(en_us.content.itapolis_text);
         
     }) 
 
